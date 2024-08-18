1c469fe46117f7c8f9153cd119a6514626c75d34
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-18 07:21:56 +0600

    write a route for publish status

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { deleteVideo, getVideoById, publishVideo, updateVideoDetails } from "../controllers/video.controller.js";
+import { deleteVideo, getVideoById, publishVideo, togglePublishStatus, updateVideoDetails } from "../controllers/video.controller.js";
 
 const router = Router()
 
@@ -34,5 +34,6 @@ router.route("/update-video-details/:videoId").patch(verifyJWT, upload.fields([
 ]), updateVideoDetails)
 
 router.route("/delete/:videoId").delete(verifyJWT, deleteVideo)
+router.route("/change-publish-status/:videoId").patch(verifyJWT, togglePublishStatus)
 
 export default router;
