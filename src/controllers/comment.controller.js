a614232751aa5e4b75fc0024f666172a132bc96d
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-19 09:28:53 +0600

    add router in comment routes file

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -3,27 +3,28 @@ import { ApiError } from "../utils/apiError";
 import { ApiResponse } from "../utils/apiResponse";
 import { asyncHandler } from "../utils/asyncHandler";
 
-const addComment = asyncHandler(async(req,res) =>{
-  const {comment, videoId} = req.body
+const addComment = asyncHandler(async (req, res) => {
+  const { comment, videoId } = req.body;
 
-  if (!comment && !videoId ) {
-    throw new ApiError(401, "Write something for comment")
+  if (!comment && !videoId) {
+    throw new ApiError(401, "Write something for comment");
   }
 
   const newComment = new Comment({
     comment,
     video: videoId,
-    owner: req.user
-  })
+    owner: req.user,
+  });
 
-  await newComment.save()
+  await newComment.save();
 
   if (!newComment) {
-    throw new ApiError(501, "Something went wrong while upload that comment")
+    throw new ApiError(501, "Something went wrong while upload that comment");
   }
 
-  return res.status(200).json(new ApiResponse(200, newComment, "Comment has been added"))
+  return res
+    .status(200)
+    .json(new ApiResponse(200, newComment, "Comment has been added"));
+});
 
-})
-
-export {addComment}
+export { addComment };
