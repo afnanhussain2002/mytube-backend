5105227955410f9e692807a527dc17cd8ca26a1c
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-19 06:30:10 +0600

    make a routes for create tweet

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,3 +1,7 @@
 import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware";
+import { createTweet } from "../controllers/tweet.controller";
 
 const router = Router()
+
+router.route("/create-tweet").post(verifyJWT, createTweet)
