86a1e8185bf0777b81d7e990fac5a426967d64b9
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-19 11:01:17 +0600

    make route for delete comment

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addComment, updateComment } from "../controllers/comment.controller.js";
+import { addComment, deleteComment, updateComment } from "../controllers/comment.controller.js";
 import { verifyOwner } from "../middlewares/owner.middleware.js";
 import { Comment } from "../models/comment.model.js";
 
@@ -8,5 +8,6 @@ const router = Router()
 
 router.route("/add-comment").post(verifyJWT, addComment)
 router.route("/update-comment/:id").patch(verifyJWT,verifyOwner(Comment), updateComment)
+router.route("/delete-comment/:id").delete(verifyJWT,verifyOwner(Comment), deleteComment)
 
 export default router
