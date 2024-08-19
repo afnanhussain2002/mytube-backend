7da5086f5c8c86bf841955162942400b16c5d474
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-19 06:57:03 +0600

    find the tweets with that user id

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -3,35 +3,49 @@ import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const createTweet = asyncHandler(async(req,res) =>{
-    // get the content
-   const {content} = req.body;
-   
-   if (!content) {
-     throw new ApiError(400, "Write some content for tweet")
-   }
-//   send to database
-   const tweet = await Tweet.create({
+const createTweet = asyncHandler(async (req, res) => {
+  // get the content
+  const { content } = req.body;
+
+  if (!content) {
+    throw new ApiError(400, "Write some content for tweet");
+  }
+  //   send to database
+  const tweet = await Tweet.create({
     content,
-    owner: req.user
-   })
+    owner: req.user,
+  });
 
-//    find that tweet
-   const createdTweet = await Tweet.findById(tweet._id)
-   
-   if (!createdTweet) {
-    throw new ApiError(500, "Something went wrong when upload the tweet")
-   }
+  //    find that tweet
+  const createdTweet = await Tweet.findById(tweet._id);
 
-   res.status(200).json(new ApiResponse(200, createdTweet, "Tweet upload successfully"))
+  if (!createdTweet) {
+    throw new ApiError(500, "Something went wrong when upload the tweet");
+  }
 
-})
+  res
+    .status(200)
+    .json(new ApiResponse(200, createdTweet, "Tweet upload successfully"));
+});
 
-const getUserTweet = asyncHandler(async(req,res) =>{
-    // get the user id
-   const {userId} = req.params;
+const getUserTweet = asyncHandler(async (req, res) => {
+  // get the user id
+  const { userId } = req.params;
 
+  //get tweets by that user id
 
-})
+  const findTweets = await Tweet.aggregate([
+    {
+        $match:{
+            "owner": new mongoose.Types.ObjectId(userId)
+        }
+    }
+  ])
+  if (!findTweets) {
+    throw new ApiError(400, "No tweets found!")
+  }
+  console.log(findTweets);
+  
+});
 
-export{createTweet, getUserTweet}
+export { createTweet, getUserTweet };
