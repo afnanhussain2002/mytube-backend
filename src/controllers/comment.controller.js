2e654445bc6089d5e69c6c775ddbb8ec67d3f6ff
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-19 11:19:12 +0600

    get the id of video

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -4,7 +4,8 @@ import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getVideoComments = asyncHandler(async(req,res) =>{
-
+    const {videoId} = req.params
+    const {page = 1, limit = 10} = req.query
 })
 
 const addComment = asyncHandler(async(req,res) =>{
