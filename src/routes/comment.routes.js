c104be638c6a21e0a512d432b581509e03edeb95
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-19 10:51:59 +0600

    fix some error while update comment

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addComment } from "../controllers/comment.controller.js";
+import { addComment, updateComment } from "../controllers/comment.controller.js";
 import { verifyOwner } from "../middlewares/owner.middleware.js";
 import { Comment } from "../models/comment.model.js";
 
 const router = Router()
 
 router.route("/add-comment").post(verifyJWT, addComment)
-router.route("/update-comment/:id").post(verifyJWT,verifyOwner(Comment), addComment)
+router.route("/update-comment/:id").patch(verifyJWT,verifyOwner(Comment), updateComment)
 
 export default router
