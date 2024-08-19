3eba7c5f546e24eb587f8cf0c9e76431a22c9f37
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-19 07:56:23 +0600

    change the verify owner middleware structure

--- src/middlewares/owner.middleware.js
+++ src/middlewares/owner.middleware.js
@@ -3,7 +3,7 @@ import { ApiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 
-export const verifyOwner = asyncHandler(async(req,_,next) =>{
+/* export const verifyOwner = asyncHandler(async(req,_,next) =>{
      try {
          const {videoId} = req.params
         const userId = req.user
@@ -21,6 +21,8 @@ export const verifyOwner = asyncHandler(async(req,_,next) =>{
             throw new ApiError(401, "You are not the owner")
         }
 
+           
+
         next()
         
 
@@ -28,4 +30,31 @@ export const verifyOwner = asyncHandler(async(req,_,next) =>{
      } catch (error) {
         throw new ApiError(401, error.message || "You are not the owner of that video")
      }
-})
+}) */
+
+     export const verifyOwner = (Model) =>{
+        return asyncHandler(async(req,_,next) =>{
+            try {
+                const {id} = req.params
+               const userId = req.user
+               
+       
+               const getOwnerData = await Model.findById(id)
+       
+               if (!getOwnerData) {
+                   throw new ApiError(400, "Data not found")
+               }
+       
+               console.log("Owner", getOwnerData.owner[0]);
+       
+               if (!userId._id.equals(getOwnerData.owner[0])) {
+                   throw new ApiError(401, "You are not the owner")
+               } 
+       
+               next()
+       
+            } catch (error) {
+               throw new ApiError(401, error.message || "You are not the owner of that video")
+            }
+        })
+     }
