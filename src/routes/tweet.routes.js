8c68445e3e926b086bee8d32151b01627c79c0c5
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-19 07:30:27 +0600

    write route for delete a tweet

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
   createTweet,
+  deleteTweet,
   getUserTweet,
   updateTweet,
 } from "../controllers/tweet.controller.js";
@@ -12,6 +13,6 @@ const router = Router();
 router.route("/create-tweet").post(verifyJWT, createTweet);
 router.route("/:userId").get(getUserTweet);
 router.route("/update/:tweetId").patch(verifyJWT,updateTweet);
-router.route("/update/:tweetId").patch(verifyJWT,deleteT);
+router.route("/delete/:tweetId").delete(verifyJWT,deleteTweet);
 
 export default router;
