fbdf2baa19e0e097a20bad7817e5efff6e76d7f7
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-19 10:45:23 +0600

    add route for update comment

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { addComment } from "../controllers/comment.controller.js";
+import { verifyOwner } from "../middlewares/owner.middleware.js";
+import { Comment } from "../models/comment.model.js";
 
 const router = Router()
 
 router.route("/add-comment").post(verifyJWT, addComment)
+router.route("/update-comment/:id").post(verifyJWT,verifyOwner(Comment), addComment)
 
 export default router
