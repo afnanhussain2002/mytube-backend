949615db3481c97d0e4259c6438f030d77bbb5cf
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-19 06:24:43 +0600

    return the tweet status with data

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,5 +1,6 @@
 import { Tweet } from "../models/tweet.model";
 import { ApiError } from "../utils/apiError";
+import { ApiResponse } from "../utils/apiResponse";
 import { asyncHandler } from "../utils/asyncHandler";
 
 const createTweet = asyncHandler(async(req,res) =>{
@@ -21,7 +22,7 @@ const createTweet = asyncHandler(async(req,res) =>{
     throw new ApiError(500, "Something went wrong when upload the tweet")
    }
 
- 
+   res.status(200).json(new ApiResponse(200, createdTweet, "Tweet upload successfully"))
 
 })
 
