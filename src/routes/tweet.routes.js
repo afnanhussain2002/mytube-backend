b644f7544f14acd9ddd20b83812ed89d09159e07
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-19 06:32:54 +0600

    fix some import issues

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { verifyJWT } from "../middlewares/auth.middleware";
-import { createTweet } from "../controllers/tweet.controller";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { createTweet } from "../controllers/tweet.controller.js";
 
 const router = Router()
 
