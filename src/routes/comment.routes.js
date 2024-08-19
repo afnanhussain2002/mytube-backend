c1a22be16fae1fdfd88856e0d6e9b8411b5ceae9
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-19 09:31:33 +0600

    add routes for comments

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware";
+import { addComment } from "../controllers/comment.controller";
 
 const router = Router()
+
+router.route("/add-comment").post(verifyJWT, addComment)
 
 export default router
