f4907c5dace312e216238cca65a2698130e881e6
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-19 11:25:45 +0600

    make a route for get video comments

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addComment, deleteComment, updateComment } from "../controllers/comment.controller.js";
+import { addComment, deleteComment, getVideoComments, updateComment } from "../controllers/comment.controller.js";
 import { verifyOwner } from "../middlewares/owner.middleware.js";
 import { Comment } from "../models/comment.model.js";
 
 const router = Router()
 
+router.route("/get-comments").get(getVideoComments)
 router.route("/add-comment").post(verifyJWT, addComment)
 router.route("/update-comment/:id").patch(verifyJWT,verifyOwner(Comment), updateComment)
 router.route("/delete-comment/:id").delete(verifyJWT,verifyOwner(Comment), deleteComment)
