9a0691f05b832b310fce7cc2b82c2c076a958879
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-19 06:18:04 +0600

    get the content for tweet

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,6 +1,14 @@
+import { ApiError } from "../utils/apiError";
 import { asyncHandler } from "../utils/asyncHandler";
 
 const createTweet = asyncHandler(async(req,res) =>{
+    // get the content
+   const {content} = req.body;
+   
+   if (!content) {
+     throw new ApiError(400, "Write some content for tweet")
+   }
+
 
 })
 
