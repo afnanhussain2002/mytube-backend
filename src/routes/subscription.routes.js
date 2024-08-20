49f1beacae77cbf4240b5f5394b8e1fe1550488b
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-20 15:24:37 +0600

    add route for toggle subscription

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { toggleSubscription } from "../controllers/subscription.controller";
+import { verifyJWT } from "../middlewares/auth.middleware";
 
 const router = Router()
+
+router.route("/channel/:id").post(verifyJWT,toggleSubscription)
 
 export default router
