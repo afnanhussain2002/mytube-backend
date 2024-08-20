b8ec54c7fc0564fe7af5ff4dee698e0b7acb4a28
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-20 15:20:22 +0600

    get the channel id

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -2,6 +2,10 @@ import { asyncHandler } from "../utils/asyncHandler";
 
 
 const toggleSubscription = asyncHandler(async(req,res) =>{
+    // get channel for subscribe
+    const {id} = req.params
+    
+    // add subscribed channel into array of object
 
 })
 
