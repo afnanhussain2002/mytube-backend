e24f16d2ef9061f980c01bc637c10a001c3ddd24
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-21 10:48:09 +0600

    get the user id who liked that video

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -3,6 +3,10 @@ import { asyncHandler } from "../utils/asyncHandler";
 
 const toggleVideoLike = asyncHandler(async(req,res) =>{
     const {videoId} = req.params
+    const likedBy =  req.user._id
+
+    
+
 })
 
 export{toggleVideoLike}
