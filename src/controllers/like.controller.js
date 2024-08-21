346e211764d8e7bde14cfe19354661db33ac7515
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-21 10:55:59 +0600

    complete the like on video toggle func

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,3 +1,5 @@
+import { Like } from "../models/like.model";
+import { ApiResponse } from "../utils/apiResponse";
 import { asyncHandler } from "../utils/asyncHandler";
 
 
@@ -5,7 +7,25 @@ const toggleVideoLike = asyncHandler(async(req,res) =>{
     const {videoId} = req.params
     const likedBy =  req.user._id
 
-    
+    // check liked already added or not
+
+    const existingLike = await Like.findById({
+        video:videoId,
+        likedBy
+    })
+
+    if (existingLike) {
+        await Like.deleteOne({_id: existingLike._id})
+        return res.status(201).json(new ApiResponse(201, existingLike, "Like withdraw successfully"))
+    }
+
+    const newLike = new Like({
+        video:videoId,
+        likedBy
+    })
+
+    await Like.save()
+    return res.status(200).json(new ApiResponse(200, newLike, "Liked successfully"))
 
 })
 
