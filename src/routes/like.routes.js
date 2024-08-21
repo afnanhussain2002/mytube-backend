dc6b9c677bb10de46c3e988f116cdc00806113f8
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-21 11:30:49 +0600

    make route for tweet like

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { toggleCommentLike, toggleVideoLike } from "../controllers/like.controller.js";
+import { toggleCommentLike, toggleTweetLike, toggleVideoLike } from "../controllers/like.controller.js";
 
 const router = Router()
 
 router.route("/video-like/:videoId").post(verifyJWT, toggleVideoLike)
 router.route("/comment-like/:commentId").post(verifyJWT, toggleCommentLike)
+router.route("/tweet-like/:tweetId").post(verifyJWT, toggleTweetLike)
 
 export default router
