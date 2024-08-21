ef1adc2d401ec79d9520100c0bfb0d9588ff6c7f
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-21 09:55:16 +0600

    make a route for get subscribed channels

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getUserChannelSubscriber, toggleSubscription } from "../controllers/subscription.controller.js";
+import { getSubscribedChannels, getUserChannelSubscriber, toggleSubscription } from "../controllers/subscription.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
 router.route("/channel/:channelId").post(verifyJWT,toggleSubscription)
 router.route("/channel-subscriber/:channelId").get(verifyJWT,getUserChannelSubscriber)
+router.route("/channel-subscribed/:subscriberId").get(verifyJWT,getSubscribedChannels)
 
 export default router
