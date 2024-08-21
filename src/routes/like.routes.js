26a7b4cf718e9a8dcfb6f290dd71505e467c9948
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-21 11:45:59 +0600

    make a route for get all liked video

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { toggleCommentLike, toggleTweetLike, toggleVideoLike } from "../controllers/like.controller.js";
+import { getLikedVideos, toggleCommentLike, toggleTweetLike, toggleVideoLike } from "../controllers/like.controller.js";
 
 const router = Router()
 
 router.route("/video-like/:videoId").post(verifyJWT, toggleVideoLike)
 router.route("/comment-like/:commentId").post(verifyJWT, toggleCommentLike)
 router.route("/tweet-like/:tweetId").post(verifyJWT, toggleTweetLike)
+router.route("/liked-videos/:likedById").get(verifyJWT, getLikedVideos)
 
 export default router
