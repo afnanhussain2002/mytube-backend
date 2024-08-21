1810fa14d007e0910da98111ba1f6b8331a8cce1
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-21 11:01:12 +0600

    setup a route for toggle video like

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware";
+import { toggleVideoLike } from "../controllers/like.controller";
 
 const router = Router()
+
+router.route("/video-like").post(verifyJWT, toggleVideoLike)
 
 export default router
