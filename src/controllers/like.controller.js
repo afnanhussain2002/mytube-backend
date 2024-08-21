df59ff2ebbc09112e32f7df4ec9911b5472e850a
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-21 11:26:09 +0600

    make route  and func for comment like

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -29,5 +29,32 @@ const toggleVideoLike = asyncHandler(async(req,res) =>{
     return res.status(200).json(new ApiResponse(200, newLike, "Liked successfully"))
 
 })
+const toggleCommentLike = asyncHandler(async(req,res) =>{
+    const {commentId} = req.params
+    const likedBy =  req.user._id
 
-export{toggleVideoLike}
+    // check liked already added or not
+
+    const existingLike = await Like.findOne({
+        comment: commentId,
+        likedBy
+    })
+
+    if (existingLike) {
+        await Like.deleteOne({_id: existingLike._id})
+        return res.status(201).json(new ApiResponse(201, existingLike, "Like withdraw successfully"))
+    }
+
+    const newLike = new Like({
+        comment: commentId,
+        likedBy
+    })
+
+    await newLike.save()
+    return res.status(200).json(new ApiResponse(200, newLike, "Liked successfully"))
+
+})
+
+
+
+export{toggleVideoLike, toggleCommentLike}
--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { toggleVideoLike } from "../controllers/like.controller.js";
+import { toggleCommentLike, toggleVideoLike } from "../controllers/like.controller.js";
 
 const router = Router()
 
 router.route("/video-like/:videoId").post(verifyJWT, toggleVideoLike)
+router.route("/comment-like/:commentId").post(verifyJWT, toggleCommentLike)
 
 export default router
