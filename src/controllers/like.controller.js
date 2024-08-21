bf1b5075afc303f1e26052f1daf2ba4857ae156c
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-21 11:29:41 +0600

    write a func for tweet like

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -54,7 +54,32 @@ const toggleCommentLike = asyncHandler(async(req,res) =>{
     return res.status(200).json(new ApiResponse(200, newLike, "Liked successfully"))
 
 })
+const toggleTweetLike = asyncHandler(async(req,res) =>{
+    const {tweetId} = req.params
+    const likedBy =  req.user._id
+
+    // check liked already added or not
+
+    const existingLike = await Like.findOne({
+        tweet:tweetId,
+        likedBy
+    })
+
+    if (existingLike) {
+        await Like.deleteOne({_id: existingLike._id})
+        return res.status(201).json(new ApiResponse(201, existingLike, "Like withdraw successfully"))
+    }
+
+    const newLike = new Like({
+        tweet:tweetId,
+        likedBy
+    })
+
+    await newLike.save()
+    return res.status(200).json(new ApiResponse(200, newLike, "Liked successfully"))
+
+})
 
 
 
-export{toggleVideoLike, toggleCommentLike}
+export{toggleVideoLike, toggleCommentLike, toggleTweetLike}
