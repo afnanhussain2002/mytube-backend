25da677c9d2564f1f7c994e24b972046f723aea5
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-22 11:01:51 +0600

    get the details of playlist

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,6 +1,9 @@
 import { asyncHandler } from "../utils/asyncHandler";
 
 const createPlaylist = asyncHandler(async(req,res) =>{
+    // get the details of playlist
+    const {name, description} = req.body;
+    
 
 })
 
