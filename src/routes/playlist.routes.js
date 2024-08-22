b00a6e3bd76f1bef1c0f7f08768aa379b931101e
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-22 11:20:12 +0600

    make a route for create playlist

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware";
+import { createPlaylist } from "../controllers/playlist.controller";
 
 const router = Router()
+
+router.route("/create-playlist").post(verifyJWT, createPlaylist)
 
 export default router;
