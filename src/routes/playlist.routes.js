1c6c2340833aa78182f85989863838df8627ae8c
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-22 11:41:04 +0600

    make route for get playlist

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createPlaylist } from "../controllers/playlist.controller.js";
+import { createPlaylist, getUserPlaylist } from "../controllers/playlist.controller.js";
 
 const router = Router()
 
 router.route("/create-playlist").post(verifyJWT, createPlaylist)
+router.route("/get-playlist").get(verifyJWT, getUserPlaylist)
 
 export default router;
