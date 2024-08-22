6b40b72434bca460f6ba65bd1f8212b60e5cad8d
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-22 14:14:20 +0600

    make a route for delete playlist

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addVideoToPlaylist, createPlaylist, getUserPlaylist, removeVideoFromPlaylist, updatePlaylist } from "../controllers/playlist.controller.js";
+import { addVideoToPlaylist, createPlaylist, deletePlaylist, getUserPlaylist, removeVideoFromPlaylist, updatePlaylist } from "../controllers/playlist.controller.js";
 import { verifyOwner } from "../middlewares/owner.middleware.js";
 import { Playlist } from "../models/playlist.model.js";
 
@@ -11,5 +11,6 @@ router.route("/get-playlist/:userId").get(verifyJWT, getUserPlaylist)
 router.route("/add-video/:playlistId/:videoId").post(verifyJWT, addVideoToPlaylist)
 router.route("/remove-video/:playlistId/:videoId").post(verifyJWT, removeVideoFromPlaylist)
 router.route("/update-playlist/:id").patch(verifyJWT, verifyOwner(Playlist), updatePlaylist)
+router.route("/delete-playlist/:id").delete(verifyJWT, verifyOwner(Playlist), deletePlaylist)
 
 export default router;
