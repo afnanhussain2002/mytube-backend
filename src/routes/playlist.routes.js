4d4b5063f567ad96040bf7c84bdb9a1c0cfd89b3
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-22 14:08:47 +0600

    make a route for update playlist

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addVideoToPlaylist, createPlaylist, getUserPlaylist, removeVideoFromPlaylist } from "../controllers/playlist.controller.js";
+import { addVideoToPlaylist, createPlaylist, getUserPlaylist, removeVideoFromPlaylist, updatePlaylist } from "../controllers/playlist.controller.js";
+import { verifyOwner } from "../middlewares/owner.middleware.js";
+import { Playlist } from "../models/playlist.model.js";
 
 const router = Router()
 
@@ -8,5 +10,6 @@ router.route("/create-playlist").post(verifyJWT, createPlaylist)
 router.route("/get-playlist/:userId").get(verifyJWT, getUserPlaylist)
 router.route("/add-video/:playlistId/:videoId").post(verifyJWT, addVideoToPlaylist)
 router.route("/remove-video/:playlistId/:videoId").post(verifyJWT, removeVideoFromPlaylist)
+router.route("/update-playlist/:id").patch(verifyJWT, verifyOwner(Playlist), updatePlaylist)
 
 export default router;
