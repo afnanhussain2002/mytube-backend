c3a1f92e5eddfbef9cc800617d537c530537522f
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-22 11:08:19 +0600

    add playlist on db

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,3 +1,4 @@
+import { Playlist } from "../models/playlist.model";
 import { ApiError } from "../utils/apiError";
 import { asyncHandler } from "../utils/asyncHandler";
 
@@ -9,7 +10,12 @@ const createPlaylist = asyncHandler(async(req,res) =>{
         throw new ApiError(401, "This playlist must have a name")
     }
 
-    
+    const savePlaylist = await Playlist.create({
+        name,
+        description: description || ""
+    })
+
+
 
 
 
