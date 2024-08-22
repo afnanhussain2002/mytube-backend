d1a160b61e4d00157147b304fa1b5cef5f3e84de
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-22 11:06:38 +0600

    add a filter for check empty playlist name

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,8 +1,14 @@
+import { ApiError } from "../utils/apiError";
 import { asyncHandler } from "../utils/asyncHandler";
 
 const createPlaylist = asyncHandler(async(req,res) =>{
     // get the details of playlist
     const {name, description} = req.body;
+
+    if (!name) {
+        throw new ApiError(401, "This playlist must have a name")
+    }
+
     
 
 
